e21acf5f42e4d7304147ae5a28b86144854eb53a
Jose Ueslei De Moraes Ferreira <60453278+uesleiMF@users.noreply.github.com>
2022-04-30 22:25:04 -0300

    sim
    d

--- src/component/Cart3/repository.js
+++
@@ -1,9 +0,0 @@
-import axios from 'axios';
-
-export function getCartProducts(cart) {
-
-    return axios.post(`http://localhost:8000/api/users2/cart`, { cart })
-
-        .then(response => response.data);
-
-}
